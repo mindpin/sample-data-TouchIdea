ba626f494530f0096a53eaae3d38e06657b3ba0d
fushang318 <fushang318@gmail.com>
2015-05-05 17:45:19 +0800

    bug fix

--- import_1_data.rb
+++ import_1_data.rb
@@ -6,12 +6,18 @@ file_1_path = File.expand_path('../1.yaml', __FILE__)
 yaml1 = YAML.load_file(file_1_path)
 p "path #{file_1_path}"
 count = yaml1.count
+infocard_error = []
 yaml1.each_with_index do |info,index|
   p "#{index+1}/#{count}"
   url = info["url"]
   URI.parse(URI.encode(url))
 
-  infocard = Infocard.parse(url)
+  begin
+    infocard = Infocard.parse(url)
+  rescue
+    infocard_error << url
+    next
+  end
 
   desc = info['desc']
   options = info['options']
@@ -28,4 +34,12 @@ yaml1.each_with_index do |info,index|
       raise "错误数据 #{url}"
     end
   end
+end
+
+p "导入完成"
+if infocard_error.count == 0
+  p "全部导入成功"
+else
+  p "有一些导入失败"
+  p infocard_error
 end
